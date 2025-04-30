f2b261f95a157b6cc617809627314b2459ec97d4
ZIDANE Abdellatif <zidane@et.esiea.fr>
2025-04-30 15:27:53 +0200

    opening port

--- app.py
+++ app.py
@@ -80,9 +80,8 @@ def ask():
     else:
         return jsonify({"answer": result["content"], "score": f"{result['score']:.2f}"})
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
     import os
-    port = int(os.environ.get("PORT", 5000))  # Obtient le port depuis la variable d'environnement ou utilise 5000 par défaut
-    app.run(host="0.0.0.0", port=port)  # Exécute l'application sur toutes les interfaces réseau disponibles
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
